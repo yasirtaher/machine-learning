4f94b838d10768eba3f672059e475f5e34781479
Yasir Taher <yasir.taher@unipro.co.jp>
2016-11-16 13:24:01 +0900

    Quiz on Naive Bayes

---
+++ Udacity-Intro to Machine Learning/#1-Naive_Bayes/02-Calculating_NB_Accuracy/classify.py
@@ -0,0 +1,21 @@
+def NBAccuracy(features_train, labels_train, features_test, labels_test):
+    """ compute the accuracy of your Naive Bayes classifier """
+    ### import the sklearn module for GaussianNB
+    from sklearn.naive_bayes import GaussianNB
+    from sklearn.metrics import accuracy_score
+
+    ### create classifier
+    clf = GaussianNB()
+
+    ### fit the classifier on the training features and labels
+    clf.fit(features_train, labels_train)
+
+    ### use the trained classifier to predict labels for the test features
+    # method 1
+    accuracy = clf.score(features_test, labels_test)
+
+    # method 2
+    pred = clf.predict(features_test)
+    accuracy = accuracy_score(pred, labels_test)
+
+    return accuracy
---
+++ Udacity-Intro to Machine Learning/#1-Naive_Bayes/02-Calculating_NB_Accuracy/studentCode.py
@@ -0,0 +1,11 @@
+from class_vis import prettyPicture
+from prep_terrain_data import makeTerrainData
+from classify import NBAccuracy
+
+features_train, labels_train, features_test, labels_test = makeTerrainData()
+
+def submitAccuracy():
+    accuracy = NBAccuracy(features_train, labels_train, features_test, labels_test)
+    return accuracy
+
+print submitAccuracy()
